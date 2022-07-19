5ed6ab96a109079ba9cb27fbb86b164edbfd356d
zilla <samyusuf101@gmail.com>
2022-07-19 15:52:41 -0700

    created thumbnail component

--- components/Rows.tsx
+++ components/Rows.tsx
@@ -1,5 +1,7 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import React from 'react'
 import { Movie } from '../typings'
+import Thumbnail from './Thumbnail'
 
 interface Props {
     title: string
@@ -8,8 +10,20 @@ interface Props {
 
 function Rows({title, movies}: Props) {
   return (
-    <div>Rows</div>
+    <div className='h-40 space-y-0.5 md:space-y-2'>
+        <h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] 
+        transition duration-200 hover:text-white md:text-2xl'>{title}</h2>
+        <div className='group relative md:ml-2'>
+            <ChevronLeftIcon className='RowIcon' />
+
+            <div>
+                <Thumbnail />
+            </div>
+            <ChevronRightIcon className='RowIcon'  />
+        </div>
+    </div>
   )
 }
+
 
 export default Rows

2f9c1fd6908cfcb3f20fc3e287d0c300e712ba95
zilla <samyusuf101@gmail.com>
2022-07-19 09:07:29 -0700

    typings.dts

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Movie } from '../typings'
+import Image from 'next/image'
+import { baseUrl } from '../constants/movie'
 
-function Banner() {
+interface Props {
+    netflixOriginals:Movie[]
+}
+function Banner({netflixOriginals}: Props) {
+    const [movie, setMovie] = useState<Movie | null>(null)
+
+    useEffect(() => {
+        setMovie(
+            netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+        )
+
+    },[netflixOriginals])
+
+    console.log(movie)
+
+
   return (
-    <div>Banner</div>
+    <div>
+        <div className='absoloute top-0 left-0 -z-10 h-[95vh] w-screen'>
+          <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} 
+          layout="fill"
+          objectFit='cover'/> 
+        </div>
+
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
 
-const Home: NextPage = () => {
+interface Props{
+  netflixOriginals  : Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({ 
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow, } : Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 
     to-[#010511] lg:h-[140vh]">
@@ -15,7 +34,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals}/>
       </main>
       
     </div>
@@ -44,5 +63,19 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ])
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+      
+    },
+  }
 
 }

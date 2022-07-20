2f246a2265317f43ae67468880896406f3066fea
zilla <samyusuf101@gmail.com>
2022-07-20 02:29:55 -0700

    created and edited row and thumbnail components

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-textshadow')
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar')
   ],
 }
 

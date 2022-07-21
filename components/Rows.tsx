e2b48f54980c894ebf66532399d265da640be285
zilla <samyusuf101@gmail.com>
2022-07-21 03:05:14 -0700

    updated rows component

--- components/Rows.tsx
+++ components/Rows.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Movie } from "../typings";
 import Thumbnail from "./Thumbnail";
 
@@ -9,6 +9,27 @@ interface Props {
 }
 
 function Rows({ title, movies }: Props) {
+    const rowRef = useRef<HTMLDivElement>(null)
+    const [isMoved, setIsisMoved] = useState(false)
+
+    const handleClick = (direction:string) => {
+        setIsisMoved(true)
+
+        if (rowRef.current) {
+            const { scrollLeft, clientWidth } = rowRef.current
+
+            const scrollTo = 
+            direction === 'left'
+            ? scrollLeft - clientWidth
+            : scrollLeft + clientWidth
+
+            rowRef.current.scrollTo ({ left: scrollTo, behavior: 'smooth'})  
+
+         }
+
+
+    }
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2
@@ -18,14 +39,14 @@ function Rows({ title, movies }: Props) {
         {title}
       </h2>
       <div className="group relative md:ml-2">
-        <ChevronLeftIcon className="RowIcon" />
+        <ChevronLeftIcon className={`RowIcon ${!isMoved && 'hidden'}`} onClick={() => handleClick("left")} />
 
-        <div className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll  md:space-x-2.5 md:p-2">
+        <div ref={rowRef} className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll  md:space-x-2.5 md:p-2">
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon className="RowIcon" />
+        <ChevronRightIcon className={`RowIcon2`}onClick={() => handleClick("right")} />
       </div>
     </div>
   );

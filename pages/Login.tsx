4397578ca8679a68c8c193fc6f4847e95f85d5e3
zilla <samyusuf101@gmail.com>
2022-07-26 07:08:04 -0700

    updated login page

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface Inputs{
+    email: string
+    password: string
+}
 
 function Login() {
     const [login, setLogin] = useState(false)
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
 
   return (
     <div className='relative flex h-screen w-screen flex-col bg-black md:items-center 
@@ -25,15 +34,21 @@ function Login() {
         height={150}
       />
 
-      <form className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+      <form onSubmit={handleSubmit(onSubmit)} className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
       md:max-w-md md:px-14'>
         <h1 className='text-4xl font-semibold'>Sign In</h1>
         <div className='space-y-4'>
             <label className='inline-block w-full'>
-                <input type="email" placeholder='Email' className='input' />
+                <input
+                 type="email" 
+                 placeholder='Email' 
+                 className='input' {...register("email", {required: true })} />
             </label>
             <label className='inline-block w-full'>
-            <input type="password" placeholder='Password' className='input'/>
+            <input 
+            type="password" 
+            placeholder='Password' 
+            className='input' {...register("password", {required: true })}/>
             </label>
         </div>
 

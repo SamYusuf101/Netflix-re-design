7e21974c483abafa54465c18b09e39075906cb37
zilla <samyusuf101@gmail.com>
2022-07-26 07:08:22 -0700

    updated login page

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,22 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form';
+import Head from "next/head";
+import Image from "next/image";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-interface Inputs{
-    email: string
-    password: string
+interface Inputs {
+  email: string;
+  password: string;
 }
 
 function Login() {
-    const [login, setLogin] = useState(false)
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async({email, password}) =>{
+    if (login) {
+        //await signIn(email, password)
+    } else {
+        //await signUp(email, password)
+    }
+  };
 
 
   return (
-    <div className='relative flex h-screen w-screen flex-col bg-black md:items-center 
-    md:justify-center md:bg-transparent'>
+    <div
+      className="relative flex h-screen w-screen flex-col bg-black md:items-center 
+    md:justify-center md:bg-transparent"
+    >
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -34,33 +46,60 @@ function Login() {
         height={150}
       />
 
-      <form onSubmit={handleSubmit(onSubmit)} className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
-      md:max-w-md md:px-14'>
-        <h1 className='text-4xl font-semibold'>Sign In</h1>
-        <div className='space-y-4'>
-            <label className='inline-block w-full'>
-                <input
-                 type="email" 
-                 placeholder='Email' 
-                 className='input' {...register("email", {required: true })} />
-            </label>
-            <label className='inline-block w-full'>
-            <input 
-            type="password" 
-            placeholder='Password' 
-            className='input' {...register("password", {required: true })}/>
-            </label>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+      md:max-w-md md:px-14"
+      >
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid email
+              </p>
+            )}
+          </label>
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Your password must contain beteen 4 and 60 characters
+              </p>
+            )}
+          </label>
         </div>
 
-        <button className='py-2 px-2 w-full bg-[#e50914] hover:bg-[#384aec] rounded font-semibold'>Sign In</button>
-        <div className='text-[gray]'>
-            New to Netflix? <br/>
-            <button type="submit" className='text-white bg-[#e50914] w-full hover:bg-[#384aec] py-2 px-2 rounded '>Sign up now</button>
+        <button
+          className="py-2 px-2 w-full bg-[#e50914] hover:bg-[#384aec] rounded font-semibold"
+          onClick={() => setLogin(true)}
+        >
+          Sign In
+        </button>
+        <div className="text-[gray]">
+          New to Netflix? <br />
+          <button
+            type="submit"
+            className="text-white bg-[#e50914] w-full hover:bg-[#384aec] py-2 px-2 rounded "
+            onClick={() => setLogin(false)}
+          >
+            Sign up now
+          </button>
         </div>
       </form>
-      
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;

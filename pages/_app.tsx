3f4cb52c1c2b5b58c3a55cad6fafae8924879420
zilla <samyusuf101@gmail.com>
2022-07-28 03:29:45 -0700

    updated useAuth hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '../hooks/useAuth'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return 
-  ( 
-    <AuthProvider>
-  <Component {...pageProps} />
-  </AuthProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp

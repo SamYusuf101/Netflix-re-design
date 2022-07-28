667f1e6aa197ce2d6f9149972292a9445fdc06f8
zilla <samyusuf101@gmail.com>
2022-07-28 14:41:55 -0700

    updated modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,10 @@
-import { PlusIcon, XIcon } from "@heroicons/react/solid";
+import {
+  PlusIcon,
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+  XIcon,
+} from "@heroicons/react/solid";
 import MuiModal from "@mui/material/Modal";
 import { useEffect, useState } from "react";
 import { FaPlay } from "react-icons/fa";
@@ -47,7 +53,7 @@ function Modal() {
     <MuiModal
       open={showModal}
       onClose={handleClose}
-      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden
+      className="fixed !top-7 left-0 right-0  z-50 mx-auto w-full md:max-w-2xl overflow-hidden
       overflow-y-scroll rounded-md scrollbar-hide"
     >
       <>
@@ -76,9 +82,56 @@ function Modal() {
                 <FaPlay className="h-7 w-7 text-black" />
                 Play
               </button>
-              <button>
-                <PlusIcon />
+
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
               </button>
+
+              <button className="modalButton">
+                <ThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div
+                className="flex h-4 items-center justify-center rounded border
+              border-white/40 px-1.5 text-xs"
+              >
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres:</span> {""}
+                  {genres.map((genre) => genre.name).join(", ")}
+                </div>
+                <div>
+                  <span className="text-[gray]">Original language:</span>
+                  {movie?.original_language}
+                </div>
+                <div>
+                  <span className="text-[gray]">Total votes:</span>
+                  {movie?.vote_count}
+                </div>
+              </div>
             </div>
           </div>
         </div>

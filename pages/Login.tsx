5a81333ea9fb4d17e9e04d8815a62df75cc65593
zilla <samyusuf101@gmail.com>
2022-07-28 03:05:16 -0700

    updated useAuth hook

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import useAuth from "../hooks/useAuth";
 
 interface Inputs {
   email: string;
@@ -10,6 +11,7 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false);
+  const {signIn, signUp} = useAuth()
   const {
     register,
     handleSubmit,
@@ -17,9 +19,9 @@ function Login() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async({email, password}) =>{
     if (login) {
-        //await signIn(email, password)
+        await signIn(email, password)
     } else {
-        //await signUp(email, password)
+        await signUp(email, password)
     }
   };
 
